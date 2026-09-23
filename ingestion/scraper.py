a0512235546404a9a5ea3aaab1de2af9a5286c08
Twanano Rikhotso <ntwananorikhotso05@gmail.com>
2026-09-23 21:54:11 +0200

    Remove hardcoded database password default

--- ingestion/scraper.py
+++ ingestion/scraper.py
@@ -54,7 +54,7 @@ def main() -> None:
     parser.add_argument(
         "--password",
         help="PostgreSQL password",
-        default="postgres",
+        required=True,
     )
 
     args = parser.parse_args()
